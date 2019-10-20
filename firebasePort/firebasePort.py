66ee302497fda46a9ca6cfd07152977ff4557398
Matthias-coding <56312272+Matthias-coding@users.noreply.github.com>
2019-10-20 12:00:41 +0200

    Add files via upload

--- firebasePort/firebasePort.py
+++ firebasePort/firebasePort.py
@@ -1,21 +1,21 @@
 import os, subprocess, pyrebase
 class Database(object):
-    def __init__(self):
-        config = {
-            "apiKey": "AIzaSyDehw4NuNihPM1Lezc1GbPGWE272yHOE1s",
-            "authDomain": "rgb-led-control.firebaseapp.com",
-            "databaseURL": "https://rgb-led-control.firebaseio.com",
-            "storageBucket": "rgb-led-control.appspot.com",
-            "tls": {
-                "rejectUnauthorized": False
-                }
-            }
+    def __init__(self, config):
+        
         self.firebase = pyrebase.initialize_app(config)
         self.db = self.firebase.database()
         
     def writeData(self, path, data):
         path = path.split('/')
+        self.db = self.firebase.database()
         for i in path:
             self.db = self.db.child(i)
         self.db.set(data)
+    def getData(self, path):
+        path = path.split('/')
+        self.db = self.firebase.database()
+        for i in path:
+            self.db = self.db.child(i)
+        return(self.db.get().val())
+        
         
